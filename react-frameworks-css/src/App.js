ff22035d7d000c5ef5580f976addb695c7855663
Andrés Alfaro Selingue <69393290+aalfaros@users.noreply.github.com>
2021-05-21 06:15:46 -0400

    fin Bootstrap y Bulma

--- react-frameworks-css/src/App.js
+++ react-frameworks-css/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import Bootstrap from './components/Bootstrap';
 import './App.css';
+import Bulma from './components/Bulma';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      <Bulma />
     </div>
   );
 }
